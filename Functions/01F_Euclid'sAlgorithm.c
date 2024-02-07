dd01684abfef2a174906c578d3e6521880670f75
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-07 21:04:40 +0100

    Funkce 1 v1.2

--- Functions/01F_Euclid'sAlgorithm.c
+++ Functions/01F_Euclid'sAlgorithm.c
@@ -1,8 +1,9 @@
+#include <inttypes.h>
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
 int maxNum(int[]);
+int Euclid_Fun(int*);
+
 
 int maxNum(int in[]) {
     int maxNum;
@@ -19,6 +20,28 @@ int maxNum(int in[]) {
 }
 
 
+int Euclid_Fun(int* minMax) {
+    
+    int a, b, d, r;
+
+    a = minMax[0];
+    b = minMax[1];
+    r = 1;
+
+    while (r != 0) {
+        r = a % b;
+        d = a / b;
+        // printf("(%d : %d)  |  %d = %d * %d + %d\n", a, b, a, d, b, r);
+        a = b;
+        b = r;
+    }
+
+    int GCD = a;
+
+    return GCD;
+}
+
+
 int main() {
 
     int i;
@@ -27,39 +50,41 @@ int main() {
     int in02[] = {765, 918};
     int in03[] = {101, 100};
 
-    int maxNum01, maxNum02, maxNum03;
+    // int* inLst[] = {&in01, &in02, &in03};
+    int* inLst[] = {in01, in02, in03};
 
-        // int maxNum(int in[])
-    maxNum01 = maxNum(in01);
-    maxNum02 = maxNum(in02);
-    maxNum03 = maxNum(in03);
+    int inNum = sizeof(inLst)/sizeof(inLst[0]);
 
-    printf("Max from in01: %d\n", maxNum01);
-    printf("Max from in02: %d\n", maxNum02);
-    printf("Max from in03: %d\n", maxNum03);
+    // printf("Size: %d\n\n", inNum);
 
-    printf("\n");
+    for (i = 0; i < inNum; i++) {
+        printf("%d.Input list: {%d, %d}\n\n", i+1, inLst[i][0], inLst[i][1]);
 
-    int a, b, r, t, d;
-    a = 60;
-    b = 36;
-    t = 1;
-    r = 60 % 36;
+        printf("\t1.Num: %d\n", inLst[i][0]);
+        printf("\t2.Num: %d\n", inLst[i][1]);
+        printf("\n");
+
+        int a, b, r, t, d;
+
+            a = inLst[i][0];
+            b = inLst[i][1];
+            t = 1;
+            r = a % b;
+
+            while (r != 0) {
+                r = a % b;
+                d = a / b;
+                printf("\t\t%d. (%d : %d)  |  %d = %d * %d + %d\n", t, a, b, a, d, b, r);
+                t += 1;
+                a = b;
+                b = r;
+            }
+            printf("\n\n");
+    }
 
     // r = a % b;
     // d = a / b;
     // printf("%d. (%d : %d)\t%d = %d * %d + %d", t, a, b, a, d, b, r);
-
-    while (r != 0) {
-        r = a % b;
-        d = a / b;
-        printf("%d. (%d : %d)  |  %d = %d * %d + %d\n", t, a, b, a, d, b, r);
-        t += 1;
-        a = b;
-        b = r;
-    }
-
-    printf("\n\n%d", r);
 
     return 0;    
 }
