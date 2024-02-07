443c4472d985e728e527cb97c2d5b510df9201b9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-07 20:12:24 +0100

    HW01 ui v1.5

--- HW01/HW01_GCD_ui.c
+++ HW01/HW01_GCD_ui.c
@@ -13,7 +13,7 @@ int Euclid_Fun(int* minMax) {
     while (r != 0) {
         r = a % b;
         d = a / b;
-        printf("(%3d : %3d)  |  %3d = %2d * %3d + %2d\n", a, b, a, d, b, r);
+        printf("\t(%3d : %3d)  |  %3d = %2d * %3d + %2d\n", a, b, a, d, b, r);
         a = b;
         b = r;
     }
@@ -27,18 +27,25 @@ int main() {
 
     int inNum[2];
     
-    scanf("%d %*c %d", &inNum[0], &inNum[1]);
-    printf("\tContent: %d / %d", inNum[0], inNum[1]);
+    // scanf("%d%*c%d", &inNum[0], &inNum[1]);
 
-    printf("\n\n");
+        printf("Input 1.num: ");
+    scanf("%d", &inNum[0]);
 
-    printf("\t\t1.Num.: %d\n", inNum[0]);
-    printf("\t\t2.Num.: %d\n", inNum[1]);
+        printf("Input 2.num: ");
+    scanf("%d", &inNum[1]);
+
+    printf("\n\tContent: %d / %d", inNum[0], inNum[1]);
+        printf("\n\n");
+
+    printf("1.Num.: %d\n", inNum[0]);
+    printf("2.Num.: %d\n", inNum[1]);
+        printf("\n");
 
         // int Euclid_Fun(int* minMax)
     int GCD = Euclid_Fun(inNum);
 
-    printf("\nGCD: %d\n\n", GCD);
+    printf("\nGCD(%d, %d) = %d\n\n", inNum[0], inNum[1], GCD);
 
     int outNum[2];
         outNum[0] = inNum[0] / GCD;
