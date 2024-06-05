3377becb118a3f3b24200107a88414a021c60959
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-05 23:37:16 +0200

    HW04 strstr v01.00 - library function

---
+++ HW04/functions/strstr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void) {
+
+    char str1[] = "Test1 Test2 Test3 Test4 Test5";
+    char str2[] = "Object1 Object2 Test1 Test2 Object3";
+
+    char fin[] = "Test2";
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    int siz1 = sizeof(str1);
+    int siz2 = sizeof(str2);
+
+    int lenFin = strlen(fin);
+    int sizFin = sizeof(fin);
+
+
+    printf("str1: %s\n", str1);
+    printf("str2: %s\n\n", str2);
+
+    printf("len1: %d\n", len1);
+    printf("siz1: %d\n\n", siz1);
+
+    printf("len2: %d\n", len2);
+    printf("siz2: %d\n\n", siz2);
+
+    printf("fin: %s\n", fin);
+
+    char* res;
+    
+    res = strstr(str1, fin);
+
+    printf("Res1: \n");
+    printf("\t        str1: %s\n", str1);
+    printf("\tRest of str1: %s\n\n", res);
+
+    res = strstr(str2, fin);
+
+    printf("Res2: \n");
+    printf("\t        str2: %s\n", str2);
+    printf("\tRest of str2: %s\n\n", res);
+
+
+    return 0;
+
+}
