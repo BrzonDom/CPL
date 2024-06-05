3630f377eead7114ea827a68b819d177baa24a58
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-06-05 23:40:27 +0200

    HW04 strstr v02.00 - my function

--- HW04/functions/strstr.c
+++ HW04/functions/strstr.c
@@ -3,22 +3,31 @@
 #include <string.h>
 
 
+size_t strlen_f(const char* str) {
+
+    size_t len = 0;
+
+    while (*str != '\0') {
+
+        len++;
+        str++;
+    }
+
+    return len;
+
+}
+
+
 int main(void) {
 
-    char str1[] = "Test1 Test2 Test3 Test4 Test5";
-    char str2[] = "Object1 Object2 Test1 Test2 Object3";
-
-    char fin[] = "Test2";
+    char str1[] = "aAbBcCdDeEfFgGhH";
+    char str2[] = "12345678901234567890";
 
     int len1 = strlen(str1);
     int len2 = strlen(str2);
 
     int siz1 = sizeof(str1);
     int siz2 = sizeof(str2);
-
-    int lenFin = strlen(fin);
-    int sizFin = sizeof(fin);
-
 
     printf("str1: %s\n", str1);
     printf("\tlen1: %d\n", len1);
@@ -27,24 +36,6 @@ int main(void) {
     printf("str2: %s\n", str2);
     printf("\tlen2: %d\n", len2);
     printf("\tsiz2: %d\n\n", siz2);
-
-    printf("fin: %s\n", fin);
-    printf("\tlenFin: %d\n", lenFin);
-    printf("\tsizFin: %d\n\n", sizFin);
-
-    char* res;
-    
-    res = strstr(str1, fin);
-
-    printf("Res1: \n");
-    printf("\t        str1: %s\n", str1);
-    printf("\tRest of str1: %s\n\n", res);
-
-    res = strstr(str2, fin);
-
-    printf("Res2: \n");
-    printf("\t        str2: %s\n", str2);
-    printf("\tRest of str2: %s\n\n", res);
 
 
     return 0;
